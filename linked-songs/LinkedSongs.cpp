bf0f21ea760fc8d969b5a8941cb8adb699fcace6
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2023-06-07 22:08:35 +0200

    Initial commit

---
+++ linked-songs/LinkedSongs.cpp
@@ -0,0 +1,76 @@
+#include "LinkedSongs.h"
+#include <string>
+
+using namespace std;
+
+HRESULT VDJ_API LinkedSongs::OnGetPluginInfo(TVdjPluginInfo8* infos)
+{
+    infos->PluginName = "LinkedSongs";
+    return S_OK;
+}
+
+HRESULT VDJ_API LinkedSongs::OnSearch(const char* search, IVdjTracksList* tracksList)
+{
+    // string url = "https://api.testprovider.com/search?query=" + Internet::urlencode(search);
+    // string html = Internet::downloadString(url);
+    // JSON json = JSON::fromString(html);
+    // list.reserve(json.children.size());
+    // for (const JSON& item : json.children)
+    // {
+    //     tracksList->add(item.uniqueId.c_str(), item.title.c_str(), item.artist.c_str(), item.remix.c_str(), nullptr, item.label.c_str(), item.comment.c_str(), item.coverUrl.c_str(), nullptr, item.length, 0, 0, 0, true, false);
+
+    // }
+    return S_OK;
+}
+
+HRESULT VDJ_API LinkedSongs::OnSearchCancel()
+{
+    // Internet::closeDownloads();
+    return S_OK;
+}
+
+HRESULT VDJ_API LinkedSongs::GetStreamUrl(const char* uniqueId, IVdjString& url, IVdjString& errorMessage)
+{
+    // string url = "https://api.testprovider.com/getStreamUrl?id=" + Internet::urlencode(uniqueId);
+    // string html = Internet::downloadString(url);
+    // JSON json = JSON::fromString(html);
+    // if (json.hasParam("url"))
+    // {
+    //     url = json.getParam("url").c_str();
+    //     return S_OK;
+    // }
+    // else
+    // {
+    //     errorMessage = json.getParam("error").c_str();
+    //     return S_FALSE;
+    // }
+
+    return S_OK;
+}
+
+HRESULT VDJ_API LinkedSongs::GetContextMenu(const char* uniqueId, IVdjContextMenu* contextMenu)
+{
+    contextMenu->add("Open in LinkedSongs");
+    return S_OK;
+}
+
+HRESULT VDJ_API LinkedSongs::OnContextMenu(const char* uniqueId, size_t menuIndex)
+{
+    // if (menuIndex==0)
+    // {
+    //     string url = "https://www.testprovider.com/" + Internet::urlencode(uniqueId);
+    //     Internet::openBrowser(url);
+    // }
+    return S_OK;
+}
+
+HRESULT VDJ_API LinkedSongs::GetFolderContextMenu(const char *folderUniqueId, IVdjContextMenu *contextMenu)
+{
+    contextMenu->add("GetFolderContextMenu");
+    return S_OK;
+}
+
+HRESULT VDJ_API LinkedSongs::OnFolderContextMenu(const char *folderUniqueId, size_t menuIndex)
+{
+    return S_OK;
+}
---
+++ linked-songs/LinkedSongs.h
@@ -0,0 +1,24 @@
+#ifndef MYPLUGIN8_H
+#define MYPLUGIN8_H
+
+// we include stdio.h only to use the sprintf() function
+// we define _CRT_SECURE_NO_WARNINGS for the warnings of the sprintf() function
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#include "vdjOnlineSource.h"
+
+class LinkedSongs : public IVdjPluginOnlineSource
+{
+public:
+    HRESULT VDJ_API OnGetPluginInfo(TVdjPluginInfo8* infos) override;
+    HRESULT VDJ_API OnSearch(const char* search, IVdjTracksList* tracksList) override;
+    HRESULT VDJ_API OnSearchCancel() override;
+    HRESULT VDJ_API GetStreamUrl(const char* uniqueId, IVdjString& url, IVdjString& errorMessage) override;
+    HRESULT VDJ_API GetContextMenu(const char* uniqueId, IVdjContextMenu* contextMenu) override;
+    HRESULT VDJ_API OnContextMenu(const char* uniqueId, size_t menuIndex) override;
+    HRESULT VDJ_API GetFolderContextMenu(const char *folderUniqueId, IVdjContextMenu *contextMenu) override;
+    HRESULT VDJ_API OnFolderContextMenu(const char* folderUniqueId, size_t menuIndex) override;
+};
+
+#endif
